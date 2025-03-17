35a0ff30e048410e566d85bd7f2608385ecd66d3
arastoo <aopdop547@gmail.com>
2025-03-17 13:46:06 +0330

    somthing changed

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DevCard_MVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Privacy");
         }
 
         public IActionResult Privacy()
